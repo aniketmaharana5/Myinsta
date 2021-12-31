6b4cc2aa9a36582e19dc0fc1fa95a9eae9a26958
Nita <nita.rani.company@gmail.com>
2021-12-31 12:01:11 +0530

    added backend

---
+++ insta-backend/src/test/java/com/example/myinstagram/MyinstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myinstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyinstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
