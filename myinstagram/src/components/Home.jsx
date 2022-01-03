f97ef06d144640c790f9a988518ef28d0d18555a
Nita <nita.rani.company@gmail.com>
2022-01-03 23:31:08 +0530

    Firebase done,get post done

--- myinstagram/src/components/Home.jsx
+++ myinstagram/src/components/Home.jsx
@@ -12,17 +12,18 @@ class Home extends Component {
     super(props);
     this.state = {
       image: null,
-      blobImage:null,
+      blobImage: null,
       progress: 0,
-      firebaseUrl:'',
+      firebaseUrl: "",
+      posts: [],
     };
     this.handlePost = this.handlePost.bind(this);
     this.onImageChange = this.onImageChange.bind(this);
     this.uploadFile = this.uploadFile.bind(this);
   }
 
-  uploadFile(img){
-    console.log(img)
+  uploadFile() {
+    let img = this.state.image;
     if (!img) return;
     const storageRef = ref(storage, `/image/${img.name}`);
     const uploadTask = uploadBytesResumable(storageRef, img);
@@ -37,57 +38,66 @@ class Home extends Component {
       },
       (err) => console.log(err),
       () => {
-        getDownloadURL(uploadTask.snapshot.ref)
-        .then((url) =>this.setState({firebaseUrl:url}),
-        console.log(this.state.firebaseUrl));
+        getDownloadURL(uploadTask.snapshot.ref).then(
+          (url) => this.setState({ firebaseUrl: url }),
+          console.log(this.state.firebaseUrl)
+        );
       }
     );
-  };
+  }
 
   handlePost(e) {
-    const userName=localStorage.getItem('userName');
-    const password=localStorage.getItem('password');
+    const data = {
+      pic: this.state.firebaseUrl,
+    };
+    const userName = localStorage.getItem("userName");
+    const password = localStorage.getItem("password");
     console.log(userName);
     console.log(password);
     e.preventDefault();
-    this.uploadFile(this.state.image);
 
-    this.timer = setTimeout(() =>
-    console.log("hiiiiiiiiiiiiiiiiiiiiiik")
-    // const data={
-    //   firebaseUrl:this.state.firebaseUrl,
-    // };
-    // console.log(this.state.firebaseUrl)
-    // axios.post(`http://localhost:8084/user/posts/${userName}`,data,
-    // { headers: {authorization: 'Basic ' + window.btoa(userName + ":" +password) } }
-    
-    , 20000);
-  
-
-    // axios.post(`http://localhost:8084/user/post/${localStorage.getItem('userName')}`)
+    console.log(this.state.firebaseUrl);
+    axios.post(`http://localhost:8084/user/posts/${userName}`, data, {
+      headers: {
+        authorization: "Basic " + window.btoa(userName + ":" + password),
+      },
+    });
+    window.location.reload(false)
   }
   onImageChange = (event) => {
     if (event.target.files && event.target.files[0]) {
       let img = event.target.files[0];
       this.setState({
         blobImage: URL.createObjectURL(img),
-        image:img
+        image: img,
       });
     }
   };
 
+  componentDidMount() {
+    const userName = localStorage.getItem("userName");
+    const password = localStorage.getItem("password");
+    axios
+      .get("http://localhost:8084/user/posts", {
+        headers: {
+          authorization: "Basic " + window.btoa(userName + ":" + password),
+        },
+      })
+      .then((res) => {
+        this.setState({ posts: res.data });
+      });
+  }
   render() {
     return (
       <div>
         <div>
-          <div className="container" >
+          <div className="container">
             <h1>Select Image</h1>
             <img
               src={this.state.blobImage}
-              style={{ width: "30em", height: "30vh",borderRadius:'2px'}}
+              style={{ width: "30em", height: "30vh", borderRadius: "2px" }}
             />
-                        <br/>
-
+            <br />
             <input
               type="file"
               name="myImage"
@@ -97,65 +107,73 @@ class Home extends Component {
             <h5>Uploaded {this.state.progress}%</h5>
             <button
               className="btn btn-primary"
+              type="button"
+              onClick={this.uploadFile}
+            >
+              Upload
+            </button>
+            &nbsp;&nbsp;
+            <button
+              className="btn btn-primary"
               type="onClick"
               onClick={this.handlePost}
             >
               Post
             </button>
+            <br />
           </div>
         </div>
-        <div className="container">
-          <div className="top_bar">
-            <div className="profile_img">
-              <img
-                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-                alt=""
-              />
-              <span>Coding BD</span>
+        {this.state.posts.map((post) => (
+          <div className="container">
+            <div className="top_bar">
+              <div className="profile_img">
+                <img
+                  src={post.profilePic}
+                  alt=""
+                />
+                <span key={post.userName}>{post.userName}</span>
+              </div>
+              <i className="fa fa-ellipsis-h"></i>
             </div>
-            <i className="fa fa-ellipsis-h"></i>
+            <div className="main_img">
+              <img src={post.pic} alt=""style={{width:'30em',height:'100vh'}} />
+            </div>
+            <div className="footer">
+              <div className="icons">
+                <div className="left_side">
+                  <i className="fa fa-heart-o" aria-hidden="true"></i>
+                  <i className="fa fa-comment-o" aria-hidden="true"></i>
+                  <i className="fa fa-paper-plane-o" aria-hidden="true"></i>
+                </div>
+                <div className="right_side">
+                  <i className="fa fa-bookmark-o" aria-hidden="true"></i>
+                </div>
+              </div>
+              <div className="likeCount">
+                <p>{post.likes}</p>
+              </div>
+              <div className="content">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis excepturi impedit facere, ad adipisci, cum veritatis
+                  libero ipsam, ex quo quis neque debitis tenetur consequatur?
+                </p>
+              </div>
+              <div className="comments">
+                <p>View All 1008 Comments</p>
+              </div>
+              <div className="comments_box">
+                <div className="icon">😊</div>
+                <div className="input_field">
+                  <input type="text" placeholder="Add a Comments..." id="" />
+                </div>
+                <div className="btn">
+                  <button>Post</button>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="main_img">
-            <img
-              src="https://images.unsplash.com/photo-1517849845537-4d257902454a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-              alt=""
-            />
-          </div>
-          <div className="footer">
-            <div className="icons">
-              <div className="left_side">
-                <i className="fa fa-heart-o" aria-hidden="true"></i>
-                <i className="fa fa-comment-o" aria-hidden="true"></i>
-                <i className="fa fa-paper-plane-o" aria-hidden="true"></i>
-              </div>
-              <div className="right_side">
-                <i className="fa fa-bookmark-o" aria-hidden="true"></i>
-              </div>
-            </div>
-            <div className="likeCount">
-              <p>10,890 Likes</p>
-            </div>
-            <div className="content">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-                excepturi impedit facere, ad adipisci, cum veritatis libero
-                ipsam, ex quo quis neque debitis tenetur consequatur?
-              </p>
-            </div>
-            <div className="comments">
-              <p>View All 1008 Comments</p>
-            </div>
-            <div className="comments_box">
-              <div className="icon">😊</div>
-              <div className="input_field">
-                <input type="text" placeholder="Add a Comments..." id="" />
-              </div>
-              <div className="btn">
-                <button onClick={this.handlePost}>Post</button>
-              </div>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     );
   }
